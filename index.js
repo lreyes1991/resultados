69e576718276503b682ec86e7f22b634b4a926ca
Luis Reyes <luisreyes18209@yahoo.com>
2022-01-24 19:07:58 -0600

    sixth mod

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host     : 'db-resultados-do-user-10689106-0.b.db.ondigitalocean.com',
-    user     : 'root',
+    user     : 'mli',
     password : 'R0joc3rez@',
     database : 'defaultdb',
     port:25060
