e5eeec31dcc3eb7db3c2315135a4d26a7ecddb9b
Luis Reyes <luisreyes18209@yahoo.com>
2022-01-26 01:35:04 -0600

    siete

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const connection = mysql.createConnection({
 var path    = require('path');
 const app = express();
 var http    = require('http').Server(app);
-app.use(express.static(path.join(__dirname, 'resultado')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.set('view engine', 'ejs' );
 
