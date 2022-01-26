a01f921cf61882bf8dd90f82a999e3f933e9c9e4
Luis Reyes <luisreyes18209@yahoo.com>
2022-01-26 13:51:51 -0600

    https

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ app.get('/resultado/:codigo',(req, res)=>{
               localStorage.setItem("orden",orden);
               }
               
-              xhr.open("GET","https://www.consultaresultadoslaboratorio.health:80/resultados/" + orden + "/" + centro,true);
+              xhr.open("GET","http://consultaresultadoslaboratorio.health:80/resultados/" + orden + "/" + centro,true);
               xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
               xhr.send();
               });
