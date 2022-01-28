93dea4721c8b196e3d7f030ff22b74bc56a799ca
Luis Reyes <luisreyes18209@yahoo.com>
2022-01-28 14:29:25 -0600

    https

--- views/js/pdf.js
+++ views/js/pdf.js
@@ -142,7 +142,7 @@ xhr.send();
     //doc.text(5, 70, RESPUESTA[0].nombreExamen);
     doc.addImage(logoigss,5,0,20,20);
     doc.fromHTML($('#target')[0],5,80,{
-      "width":500,
+      "width":600,
       "elementHandlers":specialElementHandlers
       });
     doc.save(`${localStorage.getItem("orden")}`);
