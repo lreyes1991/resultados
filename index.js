0c82f4ad75e2a6c0aee1dfdb632964a9267941a1
Luis Reyes <luisreyes18209@yahoo.com>
2022-02-01 10:12:05 -0600

    con regreso

--- index.js
+++ index.js
@@ -191,10 +191,7 @@ app.get('/resultado/:codigo',(req, res)=>{
                     var RESPUESTA  = (xhr.response);
                         RESPUESTA=JSON.parse(RESPUESTA);
                         var largo = RESPUESTA.length;
-                        var rcomentario = RESPUESTA[i].Comentario;
-                        if (rcomentario == 'null'){
-                          rcomentario = '';
-                        }
+
                     if(largo == 0){
                       
                       $('#tabla_datos').append('<p class="col-4" style="color:#FF0000;">Pendiente</p><p class="col-4 respuesta" style="color:#FF0000;">Pendiente</p><p class="col-4" style="color:#FF0000;">Pendiente</p>');
@@ -210,10 +207,16 @@ app.get('/resultado/:codigo',(req, res)=>{
 
                     }else{
                       for(let i=0;i<=largo-1;i++){
+
+                        var rcomentario = RESPUESTA[i].Comentario;
+                        if (rcomentario == 'null'){
+                          rcomentario = '';
+                        }
+
                         $('#tabla_datos').append('<p class="col-4">'+RESPUESTA[i].nombreExamen+'</p>'+
                         '<p class="col-4 respuesta ">' + RESPUESTA[i].resultado + '</p>'+
                         '<p class="col-4">'+ RESPUESTA[i].valorDeReferencia +'</p>'+
-                        '<p class="col-12" style="font-weight: bold;";>' + RESPUESTA[i].Comentario + '</p>');
+                        '<p class="col-12" style="font-weight: bold;";>' + rcomentario + '</p>');
                       }
                     }
                   }
