e3a7704f1fdcb2dbcc08e50e18910f9fb43bbeff
Luis Reyes <luisreyes18209@yahoo.com>
2022-02-08 12:53:15 -0600

    con regreso

--- index.js
+++ index.js
@@ -173,7 +173,9 @@ app.get('/resultado/:codigo',(req, res)=>{
                 
                 var orden = $("#i_orden").text();
                 var centro = '${centro}'
-                
+                if (centro != "412"){
+                  $('#infoigss').css('display','none');
+                }
 
                 var xhr = new XMLHttpRequest();
                 xhr.onreadystatechange=function(){
@@ -250,7 +252,7 @@ app.get('/resultado/:codigo',(req, res)=>{
             <p class="col-12" style="font-weight: bold;" id="pcomentario">Comentario: ${ordencomentario}</p><br>
             <p class="col-4 titulo">Prueba</p><p class="col-4 titulo">Resultado</p><p class="col-4 titulo">Referencia</p>
           </div>
-          <img src="${infoigss}"  style="width:100%;"></img>
+          <img id="infoigss" src="${infoigss}"  style="width:100%;"></img>
           </body>
           </html>`);
 	  	  }
