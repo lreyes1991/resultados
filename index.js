422925abe66fc10d8cc967a6e102f6da8e6d379e
Luis Reyes <luisreyes18209@yahoo.com>
2022-02-11 20:56:32 -0600

    con regreso2

--- index.js
+++ index.js
@@ -265,8 +265,7 @@ app.get('/resultado/:codigo',(req, res)=>{
             <p class="col-12" style="font-weight: bold;" id="pcomentario">Comentario: ${ordencomentario}</p><br>
             <p class="col-4 titulo">Prueba</p><p class="col-4 titulo">Resultado</p><p class="col-4 titulo">Referencia</p>
           </div>
-          <div class="col-12 d-flex justify-content-center">
-            </div>
+     
           </body>
           </html>`);
 	  	  }
