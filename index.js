583bbc25e80ad65e9e6b5c8e9b3b080675e9c1ce
Luis Reyes <luisreyes18209@yahoo.com>
2022-02-21 08:26:18 -0600

    todos los datos

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ VISUALIZADOR y PDF
 app.get('/pdf/:orden/:centro', (req,res)=>{
   var orden = req.params.orden;
   var centro = req.params.centro;
-  connection.query(`SELECT O.Orden,O.Centro,O.idPaciente, O.NombrePaciente,O.nombreCentro,DATE_FORMAT(O.fechaDeNacimiento, "%d/%m/%Y") as fechaDeNacimiento,O.Genero,O.Comentario as ccomentario,O.nombreOrigen,R.Comentario as rcomentario, DATE_FORMAT(O.FechaOrden, "%d/%m/%Y") as FechaOrden,R.nombreExamen,R.resultado,R.valorDeReferencia ,R.unidadMedida
+  connection.query(`SELECT O.Orden,O.Centro,O.idPaciente, O.nombreMedico, O.nombreUnidadProcedencia,O.NombrePaciente,O.nombreCentro,DATE_FORMAT(O.fechaDeNacimiento, "%d/%m/%Y") as fechaDeNacimiento,O.Genero,O.Comentario as ccomentario,O.nombreOrigen,R.Comentario as rcomentario, DATE_FORMAT(O.FechaOrden, "%d/%m/%Y") as FechaOrden,R.nombreExamen,R.resultado,R.valorDeReferencia ,R.unidadMedida
   FROM Orden as O 
   inner JOIN Resultados as R 
   ON 
